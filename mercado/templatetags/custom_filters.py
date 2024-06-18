e9081c3d46b1838873217ccf0d02ebea8a85bad3
Nicolas Colla Cameron <ns.colla@gmail.com>
2024-06-17 21:40:18 -0300

    upgrade-inicio

--- mercado/templatetags/custom_filters.py
+++ mercado/templatetags/custom_filters.py
@@ -5,3 +5,14 @@ register = template.Library()
 @register.filter(name='add_class')
 def add_class(value, arg):
     return value.as_widget(attrs={'class': arg})
+
+@register.filter
+def custom_format(value):
+    if value is None:
+        return ""
+    try:
+        value = float(value)
+    except ValueError:
+        return value
+
+    return "{:,.2f}".format(value).replace(",", "X").replace(".", ",").replace("X", ".")
--- usuarios/models.py
+++ usuarios/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Usuario(AbstractUser):
+    username = None
     email = models.EmailField(unique=True)
     telefono = models.CharField(max_length=15, blank=True, null=True)
     
