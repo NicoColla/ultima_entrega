efdd8bc0e9b321ac98700b6979c95aecadc105fc
Nicolas Colla Cameron <ns.colla@gmail.com>
2024-06-17 23:36:29 -0300

    add-editar-borrar-publicacion

--- mercado/urls.py
+++ mercado/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('publicar/', views.publicar, name='publicar'),
     path('configuracion/', views.configuracion, name='configuracion'),
+    path('configuracion/publicaciones/', views.publicaciones_usuario, name='publicaciones_usuario'),
+    path('configuracion/publicaciones/borrar/<int:publicacion_id>/', views.borrar_publicacion, name='borrar_publicacion'),
+    path('configuracion/publicaciones/editar/<int:publicacion_id>/', views.editar_publicacion, name='editar_publicacion'),
 ]
 
--- mercado/views.py
+++ mercado/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PublicarForm
 from .models import VentaVehiculo
 from django.contrib.auth.decorators import login_required
@@ -22,6 +22,28 @@ def publicar(request):
         form = PublicarForm()
     return render(request, 'barra/publicar.html', {'form': form})
 
+@login_required
+def publicaciones_usuario(request):
+    publicaciones = VentaVehiculo.objects.filter(vendedor=request.user)
+    return render(request, 'barra/configuracion/publicaciones_usuario.html', {'publicaciones': publicaciones})
+
+@login_required
+def editar_publicacion(request, publicacion_id):
+    publicacion = get_object_or_404(VentaVehiculo, id=publicacion_id, vendedor=request.user)
+    if request.method == 'POST':
+        form = PublicarForm(request.POST, request.FILES, instance=publicacion)
+        if form.is_valid():
+            form.save()
+            return redirect('mercado:publicaciones_usuario')
+    else:
+        form = PublicarForm(instance=publicacion)
+    return render(request, 'barra/configuracion/editar_publicacion.html', {'form': form})
+
+@login_required
+def borrar_publicacion(request, publicacion_id):
+    publicacion = get_object_or_404(VentaVehiculo, id=publicacion_id, vendedor=request.user)
+    publicacion.delete()
+    return redirect('mercado:publicaciones_usuario')
 
 @login_required
 def configuracion(request):
